239bd59b117b98c0f2d8956d69d683e35ab4af1f
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2023-04-19 13:24:53 -0700

    added a disconnect screen/component

--- client/src/components/MinesweeperGame/Disconnected.tsx
+++ client/src/components/MinesweeperGame/Disconnected.tsx
@@ -3,7 +3,7 @@ import '../../styles/MinesweeperGame/Disconnected.css'
 export default function Disconnected() {
   return (
     <div id='disconnected-container'>
-        <div id='disconnected-msg'>Opponent has disconnected...</div>
+        <div id='disconnected-msg'>Opponent has disconnected</div>
         <a href="/" id='disconnected'>Return to Main Menu</a>
     </div>
   )
